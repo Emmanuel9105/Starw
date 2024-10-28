10e33c855cc71717e44270f75b6268d51d8b369d
Emmanuel <emmanuelcastromartos@gmail.com>
2024-10-28 11:38:28 +0100

    nivel 3

--- Starw.py
+++ Starw.py
@@ -5,7 +5,7 @@ def inicio():
           en una nave imperial robada en una misión secreta para infiltrarse\n
           en otra estrella de la muerte que el imperio está construyendo para\n
           destruirla''')
-    input("Presiona 'Enter' para continuar...")
+    tecla = input("Presiona 'Enter' para continuar...")
     nivel1()
 
 
@@ -43,12 +43,51 @@ def nivel1():
     operador = int(input("Escribe tu respuesta: "))
     if operador == suma:
         print("Has acertadoo")
-    elif operador != suma:
-        print("Has falladoo el resultado era: " + suma)
+    else:
+        print(f"Has falladoo el resultado era: {suma}")
+        print("La galaxya esta perdida")
+        inicio()
 
+def nivel2():
+    print('''\n
+          Gracias a Chewbacca consiguen llegar al sistema correcto y ven a lo\n
+          lejos la estrella de la muerte. Como van en una nave imperial robada\n
+          se aproximan lentamente con la intención de pasar desapercibidos.\n
+          De repente suena el comunicador. “Aquí agente de espaciopuerto\n
+          P1 contactando con nave imperial P2. No están destinados en este\n
+          sector. ¿Qué hacen aquí?”. Han Solo coge el comunicador e\n
+          improvisa. “Eh… tenemos un fallo en el… eh… condensador de\n
+          fluzo... Solicitamos permiso para atracar y reparar la nave”. El\n
+          agente, que no se anda con tonterías, responde “Proporcione código\n
+          de acceso o abriremos fuego”. Han Solo ojea rápidamente el manual\n
+          del piloto que estaba en la guantera y da con la página correcta. El\n
+          código es el productorio entre el nº del agente y el nº de la nave\n
+          (ambos inclusive).''')
+    P1= randint(1,7)
+    P2 = randint(8,12)
+    producto = 1 #Empezamos por 1 porque es el elemento neutro de la multiplicacion
+    for num in range(P1, P2 + 1):
+        producto *= num
+       
+    print("P1 es: " + str(P1))
+    print("P2 es : " + str(P2))
+    print("¿Qué debe introducir?")
+    operador = int(input("Escribe tu respuesta: "))
+    
 
+    if operador == producto:
+        print("Has acertadoo")
+        print("Acceso concedido.")
+        nivel3()
+    else:
+        print("Acceso denegado")
+        print("La galaxya esta perdida.")
+        inicio()
+    
+def nivel3():
+    
          
-         
-inicio()
+# inicio()
 
-inicio()
+
+
